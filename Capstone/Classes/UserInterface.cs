cb0c23055d347402b92459e371604f155c8f06e4
Amanda Gamberale <amandagamberale@gmail.com>
2018-02-17 09:54:19 -0500

    Initial commit of completed project

---
+++ Capstone/Classes/UserInterface.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Capstone.Classes
+{
+    public class UserInterface
+    {
+        VendingMachine vendingMachine;
+
+
+        public UserInterface(VendingMachine vendingMachine)
+        {
+            this.vendingMachine = vendingMachine;
+        }
+
+
+        public void RunInterface()
+        {
+            while (true)
+            {
+                IntroPic();
+                MainMenu();
+
+            }
+
+        }
+        public void MainMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("(1) Display Vending Machine Items");
+            Console.WriteLine("(2) Purchase");
+
+            try
+            {
+                int menuChoice = int.Parse(Console.ReadLine());
+                if (menuChoice == 1)
+                {
+                    Console.WriteLine();
+                    string[] displayArray = vendingMachine.DisplayItems();
+                    foreach (string item in displayArray)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    MainMenu();
+                }
+                else if (menuChoice == 2)
+                {
+
+                    PurchaseMenu();
+                }
+                else if (menuChoice == 411)
+                {
+                    SalesReport();
+                    Console.WriteLine("*****************************");
+                    Console.WriteLine("Sales Report Printed");
+                    Console.WriteLine("*****************************");
+                    MainMenu();
+                }
+
+                else
+                {
+                    MainMenu();
+                }
+            }
+            catch (System.FormatException e)
+            {
+
+                Console.WriteLine("*****************************");
+                Console.WriteLine("Please Select A Menu Option.");
+                Console.WriteLine("*****************************");
+                MainMenu();
+            }
+
+        }
+
+        public void PurchaseMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("(1) Feed Money");
+            Console.WriteLine("(2) Select Product");
+            Console.WriteLine("(3) Finish Transaction");
+
+
+            int purchaseMenuChoice = int.Parse(Console.ReadLine());
+            if (purchaseMenuChoice == 1)
+            {
+                Console.WriteLine();
+                Console.Write("Please Insert Cash ($1, $2, $5, $10): $");
+                try
+                {
+                    int inputMoney = int.Parse(Console.ReadLine());
+                    if (inputMoney < 0)
+                    {
+                        Console.WriteLine("Balance Cannot Be Below $0.00"); // added if statement here to keep user from inputing negative money
+                    }
+                    else
+                    {
+                        vendingMachine.FeedMoney(inputMoney);
+                        Console.WriteLine();
+                        Console.WriteLine("Current Balance: $" + vendingMachine.CheckBalance());//changed to check balance because of bug with totalMoney variable
+                        PurchaseMenu();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("**********************************");
+                    Console.WriteLine("Please Enter Whole Dollar Amount.");
+                    Console.WriteLine("**********************************");
+                    PurchaseMenu();
+                }
+
+            }
+            if (purchaseMenuChoice == 2)
+            {
+                Console.WriteLine();
+                Console.Write("Please Select Product: ");
+
+
+                string inputProduct = Console.ReadLine();
+                inputProduct = inputProduct.ToUpper(); //allows user input to be case insensitive
+                int balance = vendingMachine.VendingBalance;
+                bool doesExist = vendingMachine.DoesExist(inputProduct);
+                bool isAvailable = vendingMachine.IsAvailable(inputProduct);//changed these to be in variables to make it cleaner
+                bool isEnough = vendingMachine.IsEnoughMoney(inputProduct, balance);
+
+                if (doesExist && isAvailable && isEnough)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine(vendingMachine.DispenseItem(inputProduct));
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine("Remaining Balance: $" + vendingMachine.CheckBalance());
+                    Console.WriteLine();
+                }
+                else if (!doesExist)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Sorry That Item Doesn't Exist");
+                }
+                else if (!isAvailable)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Sorry That Item Is Out of Stock. Please Select Another Item");
+                }
+                else if (!isEnough)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Insuffienct Funds. Please Insert More $");
+                }
+
+                PurchaseMenu();
+
+            }
+            if (purchaseMenuChoice == 3)
+            {
+
+                Console.WriteLine();
+                Console.WriteLine($"Your Change Is: $" + vendingMachine.CheckBalance());
+                int[] changeArray = vendingMachine.GiveChange();
+
+                Console.WriteLine("_________________________________");
+                System.Threading.Thread.Sleep(3000);
+                RunInterface();
+            }
+            else
+            {
+                PurchaseMenu();
+
+            }
+
+
+
+
+        }
+        public void IntroPic()
+        {
+
+
+            Console.WriteLine("_____________________________________________");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|############################################|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|                           |##############|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  =====  ..--''`  |~~``|   |##|````````|##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  |   |  \\     |  :    |   |##| Exact  |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  |___|   /___ |  | ___|   |##| Change |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  /=__\\  ./.__\\   |/,__\\   |##| Only   |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  \\__//   \\__//    \\__//   |##|________|##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|===========================|##############|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|```````````````````````````|##############|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#| =.._      +++     //////  |##############|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#| \\/  \\     | |     \\   \\ \\ |#|`````````|##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  \\___\\    |_|     /___ /  |#| _______ |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  / __\\  /|_|\\   // __\\    |#| |1|2|3| |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  \\__//-  \\|_//   -\\__//   |#| |4|5|6| |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|===========================|#| |7|8|9| |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|```````````````````````````|#| ``````` |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#| ..--    ______   .--._.   |#|[=======]|##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#| \\   \\   |    |   |    |   |#|  _   _  |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  \\___\\  : ___:   | ___|   |#| ||| ( ) |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  / __\\  |/ __\\   // __\\   |#| |||  `  |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|  \\__//   \\__//  /_\\__//   |#|  ~      |##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|===========================|#|_________|##|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|```````````````````````````|##############|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|############################################|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#|||||||||||||||||||||||||||||####```````###|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|#||||||||||||PUSH|||||||||||||####\\|||||/###|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|############################################|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\////////////////////////////|");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("|________________________________ | CR98 |___|");
+
+        }
+
+        public void SalesReport()
+        {
+            Console.WriteLine();
+            string[] displayArray = vendingMachine.DisplaySalesReport();
+            string salesPath = Path.Combine(Environment.CurrentDirectory, "Sales_Report.txt");
+
+            foreach (string item in displayArray)
+            {
+                using (StreamWriter sw = new StreamWriter(salesPath, true))
+                {
+                    sw.WriteLine(item);
+                }
+
+            }
+            using (StreamWriter sw = new StreamWriter(salesPath, true))
+            {
+                sw.WriteLine($"**Total Sales** {vendingMachine.TotalSales.ToString("C2")}.  Report run:  {DateTime.Now}\n\n");
+                sw.WriteLine("-------------------------------------------------------------------------");
+            }
+        }
+
+    }
+}
---
+++ Capstone/Classes/VendingMachine.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Capstone.Classes
+{
+    public class VendingMachine
+    {
+        private int vendingBalance = 0;
+        public int VendingBalance { get { return vendingBalance; } }
+        List<Item> itemList = new List<Item>();
+        private string logPath = Path.Combine(Environment.CurrentDirectory, "Vending_Machine_Log.txt");
+
+        public double TotalSales { get; set; }
+        
+        List<Item> saleList = new List<Item>();
+
+
+        public VendingMachine() //populates itemList with items after adding properties to them
+        {
+            string directory = Environment.CurrentDirectory;
+            string itemFile = "vendingmachine.csv";
+            string itemFullPath = Path.Combine(directory, itemFile);
+
+            using (StreamReader reader = new StreamReader(itemFullPath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] itemArray = line.Split('|');
+                    double moneyParsing = double.Parse(itemArray[2]);
+                    double centsAmount = moneyParsing * 100;
+                    Item tempItem = new Item();
+                    tempItem.Slot = itemArray[0];
+                    tempItem.Description = itemArray[1];
+                    tempItem.PriceInCents = (int)centsAmount;
+                    tempItem.Inventory = 5;
+                    itemList.Add(tempItem);
+                }
+            }
+        }
+        public string[] DisplayItems()
+        {
+            string[] itemArray = new string[itemList.Count];
+
+            int count = 0;
+            foreach (Item item in itemList)//loops through itemList and inserts slot, description, and $ to array to be displayed
+            {
+                int remainder = (item.PriceInCents % 100);
+                int quotient = item.PriceInCents / 100;
+                string result = quotient + "." + remainder.ToString("D2");
+                string tempstring = item.Slot + " " + item.Description + " " + "$" + result.ToString() + "| " +item.Inventory;
+                itemArray[count] = tempstring;
+                count++;
+            }
+            return itemArray;
+
+        }
+
+        public void FeedMoney(int inputMoney)//changed to void because we made check balance method
+        {
+            vendingBalance += (inputMoney * 100);
+            //need to add to log
+            using (StreamWriter sw = new StreamWriter(logPath, true))
+            {
+                sw.WriteLine($"{DateTime.Now} FEED MONEY: \t{inputMoney.ToString("C2")}\t${CheckBalance()}");
+
+            }
+            
+        }
+
+        public bool DoesExist(string inputSlotString)
+        {
+            bool result = false;
+            foreach (Item item in itemList)
+            {
+                if (inputSlotString == item.Slot)
+                {
+
+                    result = true;
+                    return result;
+
+                }
+
+            }
+            return result;
+        }
+
+        public bool IsAvailable(string inputSlotString)
+        {
+            bool result = false;
+            foreach (Item item in itemList)
+            {
+                if (inputSlotString == item.Slot)
+                {
+                    if (item.Inventory > 0)
+                    {
+                        result = true;
+                        return result;
+                    }
+                }
+
+            }
+            return result;
+        }
+
+        public bool IsEnoughMoney(string inputProduct, int fedMoney)  //in old code, we were changing bool to false if it didn't find right item on first loop.
+        {
+            bool isItEnough = false;  //changed bool to equal false, only update if it is true.  
+            foreach (Item item in itemList)
+            {
+               
+                if (inputProduct == item.Slot && fedMoney >= item.PriceInCents)
+                {
+                    isItEnough =  true;  //changes bool when it finds the correct item then breaks out of loop.
+                    break;
+                }
+            }
+            return isItEnough;
+        }
+
+        public string DispenseItem(string inputSlotString)
+        {
+            string productName = "";
+            foreach (Item item in itemList)//loop to reduce inventory by 1 and adjust vendingmachine balance
+            {
+                if (inputSlotString == item.Slot)
+                {
+                    item.Inventory--;
+                    vendingBalance -= item.PriceInCents;
+                    productName = item.Slot+": "+item.Description;
+                    
+                    //total sales for report
+                    int totalRemainder = (item.PriceInCents % 100);
+                    int totalQuotient = item.PriceInCents / 100;
+                    string totalResult = totalQuotient + "." + totalRemainder.ToString("D2");
+                    double totalResultValue = double.Parse(totalResult);
+                    TotalSales += totalResultValue;
+                    item.TotalItemSold++;
+
+                    using (StreamWriter sw = new StreamWriter(logPath, true))
+                    {
+                        int remainder = (item.PriceInCents % 100);
+                        int quotient = item.PriceInCents / 100;
+                        string result = quotient + "." + remainder.ToString("D2");
+                        
+                        sw.WriteLine($"{DateTime.Now} {item.Description} {item.Slot}\t${result}\t${CheckBalance()}");
+
+                    }
+                }
+            }
+            //add to log
+
+            string message = "";
+            if (inputSlotString.Contains("A"))
+            {
+                message = $"{productName} - Crunch Crunch, Yum!";
+            }
+            if (inputSlotString.Contains("B"))
+            {
+                message = $"{productName} -Munch Munch, Yum!";
+            }
+            if (inputSlotString.Contains("C"))
+            {
+                message = $"{productName} -Glug Glug, Yum!";
+            }
+            if (inputSlotString.Contains("D"))
+            {
+                message = $"{productName} -Chew Chew, Yum!";
+            }
+
+            return message;
+        }
+
+        public string CheckBalance()
+        {
+            string balance = "";
+            int remainder = (vendingBalance % 100);
+            int quotient = vendingBalance / 100;
+            balance = quotient + "." + remainder.ToString("D2");
+            return balance;
+        }
+
+        public int[] GiveChange()
+        {
+            string beforeBalance = CheckBalance();
+            int[] changeArray = new int[3];
+            while (vendingBalance > 0)//loop to give change by giving quarters first then dimes then nickles and reducing vending balance to 0
+            {
+                if (vendingBalance >= 25)
+                {
+                    vendingBalance -= 25;
+                    changeArray[0]++;
+                }
+                else if ((vendingBalance >= 10) && (vendingBalance < 25))
+                {
+                    vendingBalance -= 10;
+                    changeArray[1]++;
+                }
+                else if ((vendingBalance >= 5) && (vendingBalance < 10))
+                {
+                    vendingBalance -= 5;
+                    changeArray[2]++;
+                }
+            }
+
+            //adjust log
+            using (StreamWriter sw = new StreamWriter(logPath, true))
+            {
+                
+                sw.WriteLine($"{DateTime.Now} GIVE CHANGE: \t${beforeBalance}\t${CheckBalance()}");
+
+            }
+
+            return changeArray;
+        }
+
+        public string[] DisplaySalesReport()
+        {
+            string[] salesArray = new string[itemList.Count];
+
+            int count = 0;
+            foreach (Item item in itemList)//loops through itemList and inserts slot, description, and $ to array to be displayed
+            {
+                int remainder = (item.PriceInCents % 100);
+                int quotient = item.PriceInCents / 100;
+                string result = quotient + "." + remainder.ToString("D2");
+                string tempstring = item.Description + "| " + item.TotalItemSold;
+                salesArray[count] = tempstring;
+                count++;
+            }
+            return salesArray;
+
+        }
+
+    }
+
+}
